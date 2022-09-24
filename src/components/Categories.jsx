3be9763029ae772620265a4cd9373765e6a0cbbe
Kob0 <kaboom2012@mail.ru>
2022-09-25 02:23:32 +0300

    add categiries render

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
 export default function Categories() {
+  const [activeCategory, setActiveCategory] = React.useState('');
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  const onClickCategory = (index) => {
+    setActiveCategory(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((categoryName, i) => (
+          <li
+            key={i}
+            className={activeCategory === i ? 'active' : ''}
+            onClick={() => onClickCategory(i)}
+          >
+            {categoryName}
+          </li>
+        ))}
       </ul>
     </div>
   );
