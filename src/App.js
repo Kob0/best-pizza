51f7207609888bd13491afab1df4785218314b7f
Kob0 <kaboom2012@mail.ru>
2022-09-26 23:51:26 +0300

    add fetch

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
-import pizzas from './assets/pizzas.json';
-
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  fetch('https://63320c8ca54a0e83d24b5292.mockapi.io/items').then((res) =>
+    res.json().then((arr) => setItems(arr)),
+  );
+
   return (
     <div className="wrapper">
       <Header />
@@ -21,7 +25,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((item) => (
+            {items.map((item) => (
               <PizzaBlock key={item.id} {...item} />
             ))}
           </div>
