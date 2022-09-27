8fb1a7871b15344921b5e6d4073a5f45129eabcd
Kob0 <kaboom2012@mail.ru>
2022-09-27 19:18:55 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Cart from './pages/Cart';
 
 import './scss/app.scss';
 
@@ -9,7 +13,13 @@ function App() {
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container"></div>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/Cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
