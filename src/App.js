e9d139c379d83c63f8785ff9f1d2db6f38fa7bb0
Kob0 <kaboom2012@mail.ru>
2022-09-27 16:33:02 +0300

    replace home-page data

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import React from 'react';
 
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
-import Skeleton from './components/PizzaBlock/Skeleton';
 
 import './scss/app.scss';
 
 function App() {
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    fetch('https://63320c8ca54a0e83d24b5292.mockapi.io/items')
-      .then((res) => res.json())
-      .then((arr) => {
-        setItems(arr);
-      });
-    setIsLoading(false);
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-              : items.map((item) => <PizzaBlock key={item.id} {...item} />)}
-          </div>
-        </div>
+        <div className="container"></div>
       </div>
     </div>
   );
