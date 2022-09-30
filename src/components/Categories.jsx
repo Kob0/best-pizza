5a70a03a31f15c47fc4a6f3df4711a4d349930d1
Kob0 <kaboom2012@mail.ru>
2022-09-30 23:47:10 +0300

    refactor with props

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 
-export default function Categories() {
-  const [activeCategory, setActiveCategory] = React.useState(0);
+export default function Categories({ value, onChangeCategory }) {
   const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 
   return (
     <div className="categories">
       <ul>
         {categories.map((categoryName, i) => (
-          <li
-            key={i}
-            className={activeCategory === i ? 'active' : ''}
-            onClick={() => setActiveCategory(i)}
-          >
+          <li key={i} className={value === i ? 'active' : ''} onClick={() => onChangeCategory(i)}>
             {categoryName}
           </li>
         ))}
