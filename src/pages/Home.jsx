ff53d837bf3d3614a449a7c4678fa52987c05dd6
Kob0 <kaboom2012@mail.ru>
2022-09-30 23:47:45 +0300

    add props for sort and categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,29 +10,39 @@ import '../scss/app.scss';
 const Home = () => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortProps, setSortProps] = React.useState({
+    name: 'популярности',
+    sortType: 'rating',
+    sortOrder: 'asc',
+  });
+
+  const category = categoryId > 0 ? categoryId : '';
 
   React.useEffect(() => {
-    fetch('https://63320c8ca54a0e83d24b5292.mockapi.io/items')
+    setIsLoading(true);
+    fetch(
+      `https://63320c8ca54a0e83d24b5292.mockapi.io/items?category=${category}&sortBy=${sortProps.sortType}`,
+    )
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [category, sortProps]);
+
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+  const pizzas = items.map((item) => <PizzaBlock key={item.id} {...item} />);
 
   return (
     <>
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories value={categoryId} onChangeCategory={(id) => setCategoryId(id)} />
+        <Sort value={sortProps} onChangeSort={(i) => setSortProps(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((item) => <PizzaBlock key={item.id} {...item} />)}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
     </>
   );
 };
