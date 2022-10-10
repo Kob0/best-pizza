2db794b986956ae402812b000e95891fb0ab7e1b
Kob0 <kaboom2012@mail.ru>
2022-10-10 22:59:07 +0300

    fix name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,20 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination';
+
+import { SearchContext } from '../App';
 
 import '../scss/app.scss';
 
 const Home = () => {
+  const { searchValue } = React.useContext(SearchContext);
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
   const [sortData, setSortData] = React.useState({
-    name: 'популярности',
+    name: 'популярности (убывание)',
     sortType: 'rating',
   });
 
@@ -23,9 +28,10 @@ const Home = () => {
 
     const sortOrder = sortData.sortType.includes('-') ? 'desc' : 'asc';
     const sortBy = sortData.sortType.replace('-', '');
+    const search = searchValue ? `&search=${searchValue}` : '';
 
     fetch(
-      `https://633ad404e02b9b64c6187dd2.mockapi.io/pizzas?${category}&sortBy=${sortBy}&order=${sortOrder}`,
+      `https://633ad404e02b9b64c6187dd2.mockapi.io/pizzas?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${sortOrder}${search}`,
     )
       .then((res) => res.json())
       .then((arr) => {
@@ -33,7 +39,7 @@ const Home = () => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [category, sortData]);
+  }, [category, sortData, searchValue, currentPage]);
 
   const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
   const pizzas = items.map((item) => <PizzaBlock key={item.id} {...item} />);
@@ -46,6 +52,7 @@ const Home = () => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination currentPage={currentPage} onChangePage={(number) => setCurrentPage(number)} />
     </>
   );
 };
