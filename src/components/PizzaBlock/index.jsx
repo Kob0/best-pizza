83721e49aaa40624ff81c146a9914a4fd489e786
Kob0 <kaboom2012@mail.ru>
2022-10-13 00:03:35 +0300

    add redux logic

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItem } from '../../Redux/slices/cartSlice';
 
-export default function PizzaBlock({ title, price, imageUrl, sizes, types }) {
+const typeNames = ['тонкое', 'традиционное'];
+
+const PizzaBlock = ({ id, title, price, imageUrl, sizes, types }) => {
+  const dispatch = useDispatch();
+  const cartItem = useSelector((state) => state.cart.items.find((obj) => obj.id === id));
   const [activeSize, setActiveSize] = React.useState(0);
   const [activeType, setActiveType] = React.useState(0);
-  const typeNames = ['тонкое', 'традиционное'];
 
+  const addedCount = cartItem ? cartItem.count : 0;
+  const onClickAdd = () => {
+    const item = {
+      id,
+      title,
+      price,
+      imageUrl,
+      size: sizes[activeSize],
+      type: typeNames[activeType],
+    };
+
+    dispatch(addItem(item));
+  };
   return (
     <div className="pizza-block">
       <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
@@ -15,8 +33,7 @@ export default function PizzaBlock({ title, price, imageUrl, sizes, types }) {
             <li
               key={typeId}
               onClick={() => setActiveType(typeId)}
-              className={activeType === typeId ? 'active' : ''}
-            >
+              className={activeType === typeId ? 'active' : ''}>
               {typeNames[typeId]}
             </li>
           ))}
@@ -26,8 +43,7 @@ export default function PizzaBlock({ title, price, imageUrl, sizes, types }) {
             <li
               key={i}
               onClick={() => setActiveSize(i)}
-              className={activeSize === i ? 'active' : ''}
-            >
+              className={activeSize === i ? 'active' : ''}>
               {size} см
             </li>
           ))}
@@ -35,23 +51,23 @@ export default function PizzaBlock({ title, price, imageUrl, sizes, types }) {
       </div>
       <div className="pizza-block__bottom">
         <div className="pizza-block__price">от {price} ₽</div>
-        <div className="button button--outline button--add">
+        <div onClick={onClickAdd} className="button button--outline button--add">
           <svg
             width="12"
             height="12"
             viewBox="0 0 12 12"
             fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+            xmlns="http://www.w3.org/2000/svg">
             <path
               d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
-              fill="white"
-            ></path>
+              fill="white"></path>
           </svg>
           <span>Добавить</span>
-          <i>0</i>
+          {addedCount > 0 && <i>{addedCount}</i>}
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default PizzaBlock;
