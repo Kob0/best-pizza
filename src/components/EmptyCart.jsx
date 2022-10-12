d643e014471334e482f9ea0be169402e000c39f7
Kob0 <kaboom2012@mail.ru>
2022-10-13 00:04:00 +0300

    create component

---
+++ src/components/EmptyCart.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import emptyCartImg from '../assets/img/empty-cart.png';
+
+const EmptyCart = () => {
+  return (
+    <>
+      <div className="cart cart--empty">
+        <h2>
+          Корзина пуста <span>😕</span>
+        </h2>
+        <p>
+          Вероятнее всего, вы еще не заказывали пиццу.
+          <br />
+          Для оформления заказа перейдите на главную страницу.
+        </p>
+        <img src={emptyCartImg} alt="Empty cart" />
+        <Link to="/" className="button button--black">
+          <span>Вернуться назад</span>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default EmptyCart;
