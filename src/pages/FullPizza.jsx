7f9b4b577af0edb2ba1c4ec3c0ca1626c7a2181b
Kob0 <kaboom2012@mail.ru>
2022-10-19 00:10:18 +0300

    add component

---
+++ src/pages/FullPizza.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+
+const FullPizza = () => {
+  const [pizza, setPizza] = React.useState();
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    async function fetchPizza() {
+      try {
+        const { data } = await axios.get(
+          'https://633ad404e02b9b64c6187dd2.mockapi.io/pizzas/' + id,
+        );
+        setPizza(data);
+      } catch (error) {
+        alert('Ошибка при получении пиццы!');
+        navigate('/');
+      }
+    }
+
+    fetchPizza();
+  }, []);
+
+  if (!pizza) {
+    return 'Загрузка...';
+  }
+
+  return (
+    <div className="container">
+      <img src={pizza.imageUrl} alt="Пицца" />
+      <h2>{pizza.title}</h2>
+      <h4>{pizza.price} ₽</h4>
+    </div>
+  );
+};
+
+export default FullPizza;
