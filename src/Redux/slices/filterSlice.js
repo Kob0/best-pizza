68d40e1d81d0c1bb88713b58790514735e385935
Kob0 <kaboom2012@mail.ru>
2022-10-19 00:11:45 +0300

    add selectors

--- src/Redux/slices/filterSlice.js
+++ src/Redux/slices/filterSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
+  searchValue: '',
   categoryId: 0,
   currentPage: 1,
   sortData: {
@@ -16,23 +17,27 @@ const filterSlice = createSlice({
     setCategoryId(state, action) {
       state.categoryId = action.payload;
     },
-
+    setSearchValue(state, action) {
+      state.searchValue = action.payload;
+    },
     setSortData(state, action) {
       state.sortData = action.payload;
     },
-
     setCurrentPage(state, action) {
       state.currentPage = action.payload;
     },
-
     setFilters(state, action) {
       state.currentPage = Number(action.payload.currentPage);
       state.categoryId = Number(action.payload.categoryId);
-      state.sort = action.payload.sort;
+      state.sortData = action.payload.sortData;
     },
   },
 });
 
-export const { setCategoryId, setSortData, setCurrentPage, setFilters } = filterSlice.actions;
+export const selectFilters = (state) => state.filter;
+export const selectSortData = (state) => state.filter.sortData;
+
+export const { setCategoryId, setSortData, setCurrentPage, setFilters, setSearchValue } =
+  filterSlice.actions;
 
 export default filterSlice.reducer;
