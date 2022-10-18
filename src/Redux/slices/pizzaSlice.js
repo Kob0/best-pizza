a70bbab1fc92693f8803cc56488310c21ecbf30b
Kob0 <kaboom2012@mail.ru>
2022-10-19 00:09:29 +0300

    add slice

---
+++ src/Redux/slices/pizzaSlice.js
@@ -0,0 +1,40 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchPizzas = createAsyncThunk('pizza/fetchPizzasStatus', async (params, thunkApi) => {
+  const { category, sortOrder, sortBy, search, currentPage } = params;
+  const { data } = await axios.get(
+    `https://633ad404e02b9b64c6187dd2.mockapi.io/pizzas?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${sortOrder}${search}`,
+  );
+
+  return data;
+});
+
+const initialState = {
+  items: [],
+  status: 'loading',
+};
+
+const pizzaSlice = createSlice({
+  name: 'pizza',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchPizzas.pending]: (state) => {
+      state.status = 'loading';
+      state.items = [];
+    },
+    [fetchPizzas.fulfilled]: (state, action) => {
+      state.items = action.payload;
+      state.status = 'success';
+    },
+    [fetchPizzas.rejected]: (state) => {
+      state.status = 'error';
+      state.items = [];
+    },
+  },
+});
+
+export const selectPizzaData = (state) => state.pizza;
+
+export default pizzaSlice.reducer;
